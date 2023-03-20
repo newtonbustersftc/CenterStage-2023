f201ffb3c229ae4a2949befd3ed6e7cff9720feb
Newton Busters <newtonbustersftc@gmail.com>
2023-03-19 20:25:39 -0500

    modification, add motion profile for turret with Road Runner's output

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/ManualFeedforwardTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/ManualFeedforwardTuner.java
@@ -7,6 +7,8 @@ import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kA;
 import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kStatic;
 import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kV;
 
+import android.os.Environment;
+
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
@@ -14,6 +16,7 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.kinematics.Kinematics;
 import com.acmerobotics.roadrunner.profile.MotionProfile;
 import com.acmerobotics.roadrunner.profile.MotionProfileGenerator;
+import com.acmerobotics.roadrunner.profile.MotionSegment;
 import com.acmerobotics.roadrunner.profile.MotionState;
 import com.acmerobotics.roadrunner.util.NanoClock;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -22,13 +25,17 @@ import com.qualcomm.robotcore.util.RobotLog;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Logger;
-import org.firstinspires.ftc.teamcode.RobotFactory;
 import org.firstinspires.ftc.teamcode.RobotHardware;
 import org.firstinspires.ftc.teamcode.RobotProfile;
 import org.firstinspires.ftc.teamcode.drive.NBMecanumDrive;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.drive.opmode1.TurretMotionProfileGenerator;
 
 import java.io.File;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Objects;
 
 /*
@@ -49,12 +56,14 @@ import java.util.Objects;
 @Config
 @Autonomous(group = "drive")
 public class ManualFeedforwardTuner extends LinearOpMode {
-    public static double DISTANCE = 72; // in
+
+    public static int TOTAL_REC_CNT = 50000;
+    public static double DISTANCE = 60; // in
 
     private FtcDashboard dashboard = FtcDashboard.getInstance();
 
     private SampleMecanumDrive drive;
-
+    RobotHardware robotHardware;
     enum Mode {
         DRIVER_MODE,
         TUNING_MODE
@@ -65,7 +74,9 @@ public class ManualFeedforwardTuner extends LinearOpMode {
     private static MotionProfile generateProfile(boolean movingForward) {
         MotionState start = new MotionState(movingForward ? 0 : DISTANCE, 0, 0, 0);
         MotionState goal = new MotionState(movingForward ? DISTANCE : 0, 0, 0, 0);
-        return MotionProfileGenerator.generateSimpleMotionProfile(start, goal, MAX_VEL, MAX_ACCEL);
+//        return MotionProfileGenerator.generateSimpleMotionProfile(start, goal, MAX_VEL, MAX_ACCEL);
+        return new TurretMotionProfileGenerator().generateSimpleMotionProfile(start, goal, MAX_VEL, MAX_ACCEL);
+
     }
 
     @Override
@@ -83,7 +94,9 @@ public class ManualFeedforwardTuner extends LinearOpMode {
 
         Logger.init();
         Telemetry telemetry = new MultipleTelemetry(this.telemetry, dashboard.getTelemetry());
-        RobotHardware robotHardware = RobotFactory.getRobotHardware(hardwareMap, robotProfile);
+        robotHardware = new RobotHardware();
+        robotHardware.init(hardwareMap, robotProfile);
+
         robotHardware.resetDriveAndEncoders();
         robotHardware.resetImu();
         drive = (NBMecanumDrive)robotHardware.getMecanumDrive();
@@ -103,7 +116,12 @@ public class ManualFeedforwardTuner extends LinearOpMode {
         boolean movingForwards = true;
         MotionProfile activeProfile = generateProfile(true);
         double profileStart = clock.seconds();
-
+        Logger.logFile("profile has " + activeProfile.getSegments().size() + " segments");
+        for(int i=0; i<activeProfile.getSegments().size(); i++){
+            MotionSegment segment = activeProfile.getSegments().get(i);
+            Logger.logFile("start X="+segment.getStart().getX()+", V="+segment.getStart().getV());
+            Logger.logFile("segment dt="+segment.getDt());
+        }
 
         while (!isStopRequested()) {
             telemetry.addData("mode", mode);
@@ -125,13 +143,21 @@ public class ManualFeedforwardTuner extends LinearOpMode {
                     }
 
                     MotionState motionState = activeProfile.get(profileTime);
+                    Logger.logFile("profileTime="+ profileTime);
+                    Logger.logFile(motionState.toString());
                     double targetPower = Kinematics.calculateMotorFeedforward(motionState.getV(), motionState.getA(), kV, kA, kStatic);
+                    Logger.logFile("power="+targetPower);
 
                     drive.setDrivePower(new Pose2d(targetPower, 0, 0));
                     drive.updatePoseEstimate();
 
                     Pose2d poseVelo = Objects.requireNonNull(drive.getPoseVelocity(), "poseVelocity() must not be null. Ensure that the getWheelVelocities() method has been overridden in your localizer.");
                     double currentVelo = poseVelo.getX();
+
+                    robotHardware.clearBulkCache();
+                    recording[loop] = new Record(profileTime,currentVelo,drive.getPoseEstimate().getX() );
+                    recording_RR_profile[loop] = new Record_mp(profileTime, targetPower, motionState.getV(), (int)motionState.getX(), motionState.getA());
+                    loop++;
 
                     // update telemetry
                     telemetry.addData("targetVelocity", motionState.getV());
@@ -157,6 +183,70 @@ public class ManualFeedforwardTuner extends LinearOpMode {
             }
 
             telemetry.update();
+        }
+        writeResult();
+    }
+
+    int loop = 0;
+
+    class Record {
+        double ts;  double velocity; double position;
+        public Record(double ts, double velocity, double position) {
+            this.ts = ts;
+            this.velocity = velocity;
+            this.position = position;
+        }
+
+        public String toString() {
+            return "" + ts +  "," + velocity + "," + position;
+        }
+    }
+
+    class Record_mp {
+        double ts; double power; double velocity; int position; double acceleration;
+        public Record_mp(double ts, double power, double velocity, int position, double acceleration) {
+            this.ts = ts;
+            this.power = power;
+            this.velocity = velocity;
+            this.position = position;
+            this.acceleration = acceleration;
+        }
+
+        public String toString() {
+            return "" + ts + "," + power + "," + velocity + "," + position +","+acceleration;
+        }
+    }
+
+    Record[] recording = new Record[TOTAL_REC_CNT];
+    Record_mp[] recording_RR_profile = new Record_mp[TOTAL_REC_CNT];
+
+    private void writeResult(){
+        String timestamp = new SimpleDateFormat("yyyyMMdd-HHmm", Locale.US).format(new Date());
+        try {
+            PrintWriter pw = new PrintWriter(new File(Environment.getExternalStorageDirectory().getPath() + "/FIRST/turdat_" + timestamp + ".csv"));
+            pw.println("ts,power,velocity,position");
+            for(int i=0; i<loop; i++){
+                pw.println(recording[i]);
+            }
+            pw.flush();
+            pw.close();
+
+            //test, print the second set of data from motion state
+            PrintWriter pw_mp = new PrintWriter(new File(Environment.getExternalStorageDirectory().getPath() + "/FIRST/turdat_RR" + timestamp + ".csv"));
+            pw_mp.println("ts,power,velocity,position");
+            for(int i=0; i<loop; i++){
+                pw_mp.println(recording_RR_profile[i]);
+            }
+            pw_mp.flush();
+            pw_mp.close();
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        try {
+            Logger.flushToFile();
+        }
+        catch (Exception ex) {
         }
     }
 }
