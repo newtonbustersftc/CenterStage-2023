d72a3148ddbb67ef2b5ad45f2513fd5c74e0d769
laubarbara3 <laubarbara3@gmail.com>
2023-11-06 23:29:16 -0600

    modified the motors'and encoders' name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -52,8 +52,8 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
 //        rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "FRMotor"));
 //        frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "FLMotor"));
         leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "RLMotor"));
-        rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "RRMotor"));
-        frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "FRMotor"));
+        rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "FRMotor"));
+        frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "RRMotor"));
         // TODO: reverse any encoders using Encoder.setDirection(Encoder.Direction.REVERSE)
         if (profile.hardwareSpec.leftEncoderReverse) {
             leftEncoder.setDirection(Encoder.Direction.REVERSE);
