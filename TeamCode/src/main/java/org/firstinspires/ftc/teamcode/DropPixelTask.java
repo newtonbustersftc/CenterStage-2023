5dc8c08d35c215605a126b997323ad5585cc21db
NewtonBustersFTC <newtonbustersftc@gmail.com>
2023-11-25 18:08:49 -0600

    Auto drop corner situation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DropPixelTask.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DropPixelTask.java
@@ -27,29 +27,37 @@ public class DropPixelTask implements RobotControl {
             // use distance sensor and P(id) to reach desired distance
             double dLeft = robotHardware.getDistanceSensorLeft();
             double dRight = robotHardware.getDistanceSensorRight();
-            // when it's too far, this drop will not happen
-            if (dLeft > profile.hardwareSpec.autoDropMaxDist + profile.hardwareSpec.leftOffsetDist ||
+            // when both side too far, this drop will not happen
+            if (dLeft > profile.hardwareSpec.autoDropMaxDist + profile.hardwareSpec.leftOffsetDist &&
                 dRight > profile.hardwareSpec.autoDropMaxDist) {
                 mode = Mode.DONE;
                 Logger.logFile("DropPixel too far, Left: " + dLeft + " Right: " + dRight);
             }
+            else if (dLeft > profile.hardwareSpec.autoDropMaxDist + profile.hardwareSpec.leftOffsetDist &&
+                    dRight < profile.hardwareSpec.dropPixelDist ||
+                    dRight > profile.hardwareSpec.autoDropMaxDist &&
+                            dLeft < profile.hardwareSpec.dropPixelDist + profile.hardwareSpec.leftOffsetDist ) {
+                // when only one side is too far, one side is in distance -> Corner situation
+                mode = Mode.OPEN;
+                Logger.logFile("DropPixel corner situation, Left: " + dLeft + " Right: " + dRight);
+            }
             else {
                 if (dLeft < profile.hardwareSpec.dropPixelDist + profile.hardwareSpec.leftOffsetDist &&
                         dRight < profile.hardwareSpec.dropPixelDist) {
-                    mode = Mode.OPEN;   // testing
-                    robotHardware.setMotorPower(0, 0, 0, 0);
-                    robotHardware.grabberOpen();
-                    dropTime = System.currentTimeMillis();
+                    mode = Mode.OPEN;
                     Logger.logFile("DropPixel Close enough, Left: " + dLeft + " Right: " + dRight);
                 } else {
                     boolean isStop = powerMotorByDist(dLeft, profile.hardwareSpec.dropPixelDist + profile.hardwareSpec.leftOffsetDist, dRight, profile.hardwareSpec.dropPixelDist);
                     if (isStop) {
-                        mode = Mode.OPEN;  // testing
-                        robotHardware.grabberOpen();
-                        dropTime = System.currentTimeMillis();
+                        mode = Mode.OPEN;
                         Logger.logFile("DropPixel Power Min, Left: " + dLeft + " Right: " + dRight);
                     }
                 }
+            }
+            if (mode==Mode.OPEN) {
+                robotHardware.setMotorPower(0, 0, 0, 0);
+                robotHardware.grabberOpen();
+                dropTime = System.currentTimeMillis();
             }
         }
         if (mode==Mode.OPEN && (System.currentTimeMillis() - dropTime) > 200) {
